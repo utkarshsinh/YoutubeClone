e87ff139ecbf0c949700b208e6fa9c2e6142343b
utkarshsinh <usinh002@ucr.edu>
2023-11-22 13:42:14 -0800

    Initial commit

---
+++ yt-web-client/app/page.tsx
@@ -0,0 +1,23 @@
+import styles from './page.module.css';
+import Link from 'next/link';
+import Image from 'next/image';
+import { getVideos } from './firebase/functions';
+
+export default async function Home() {
+  const videos = await getVideos();
+
+  return (
+    <main>
+      {
+        videos.map((video) => (
+          <Link href={`/watch?v=${video.filename}`} key={video.id}>
+            <Image src={'/thumbnail.png'} alt='video' width={120} height={80}
+              className={styles.thumbnail}/>
+          </Link>
+        ))
+      }
+    </main>
+  )
+}
+
+export const revalidate = 30;
